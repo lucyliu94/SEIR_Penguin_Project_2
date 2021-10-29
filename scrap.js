6c87f8bc35a5a379a003d2395bf15791b948f751
Lucy Liu <lucy.liu94@gmail.com>
2021-10-29 14:44:01 -0400

    added cards to index page & carousel

---
+++ scrap.js
@@ -0,0 +1,25 @@
+// // IN MY LIQUID.INDEX
+
+// // <!-- 
+//   <div id ="animal-list">
+//     {% for animal in animals %}
+//         <article>
+//             <div>
+//                 <a href="/animals/{{ animal._id }}">
+//                 <h2>{{animal.name}}</h2>
+//                 <h3>{{animal.species}}</h3>
+//             </div>
+//            <div>
+//             <ul>
+//                 <img src="{{animal.img}}"> 
+//                 <li>Gender: {{animal.gender}}</li>
+//                 <li>Age: {{animal.age}}</li>
+//                 <li>Breed: {{animal.breed}}</li>
+//                 <li>Neutered/Spayed: {{animal.neuteredSpayed}}</li>
+//                 <li>On-Hold(pending-foster): {{animal.onHold}}</li>
+//             </ul>
+//            </div>
+             
+//         </article>
+// //     {% endfor %}
+// // </div> -->
